ebe126e0991cffd506a845939685fb5347ccc13f
Senac Hub - Sala 203 <hubsala203@aluno.ms.senac.br>
2025-05-30 21:15:35 -0400

    Second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,88 @@
+// 1 - CRIAR UMA VARIAVEL DO TIPO LIST
+// COM 5 ITENS DE PREFERENCIA String
+// USAR O LISTVIEW.BUILDER PARA MOSTRAR
+// OS VALORES DO ARRAY NA TELA
+
+// 2 - COLOCAR DOIS ICONES UM DO LADO DO OUTRO
+// Exemplo de Icone: Icon(Icons.person)
+// e embaixo um texto qualquer
+
+// 3 - Quero uma variavel do tipo int
+// quero que voces mostrem o valor dessa variavel na TELA
+// e tenha um botao embaixo
+// quero que criem um metodo que toda vezes que eu clicar no botao
+// a variavel tenha uma incremento de 1
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MeuApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-
+class MeuApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
- 
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class _MyAppState extends State<MeuApp> {
+  final List<String> items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
+  int counter = 0;
 
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void incrementCounter() {
     setState(() {
-
-      _counter++;
+      counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Caio Fagundes"),
-        backgroundColor: Colors.amber,
-        centerTitle: true,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Test flutter')),
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(items[index]),
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    Icon(Icons.person, size: 50),
+                    Text("Usuário 1"),
+                  ],
+                ),
+                SizedBox(width: 20),
+                Column(
+                  children: [
+                    Icon(Icons.person, size: 50),
+                    Text("Usuário 2"),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Valor do contador: $counter',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: incrementCounter,
+              child: Text('Incrementar'),
+            ),
+          ],
+        ),
       ),
-    body: Center(child: Text("Caio Fagundes",)),
-    
-
-
     );
-    
   }
 }
